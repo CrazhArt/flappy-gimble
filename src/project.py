bd72f64a49958f92c173e5296ffeed673244c7da
Crazh <atzoova@gmail.com>
2026-04-25 15:30:09 -0500

    Code posistioning and removed def main()

--- src/project.py
+++ src/project.py
@@ -14,13 +14,8 @@ SCREEN_HEIGHT = 1080
 FPS = 60
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-GAP = 300
+GAP = 350
 SPAWN_INTERVAL = 2250 # Interval in milliseconds
-
-scrollSpeed = 20
-
-flying = False
-gameOver = False
 
 black = (0, 0 ,0)
 
@@ -47,13 +42,11 @@ def resetGame():
 
 class Gimble(pygame.sprite.Sprite):
     def __init__(self, x_coord, y_coord):
-        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
 
         self.sprite_frames = []
         self.index = 0
         self.counter = 0
-        self.velocity = 0
-        self.pressed = False
 
         for i in range(1,5):
             image = pygame.image.load(f"Assets/Player/defaultCHAR{i}.png")
@@ -62,18 +55,21 @@ class Gimble(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (x_coord, y_coord)
 
+        self.velocity = 0
+        self.pressed = False
+
     def update(self):
         if flying == True:
-            self.velocity += 0.5
-            if self.velocity > 8.5:
-                self.velocity = 8.5
+            self.velocity += 2
+            if self.velocity > 20:
+                self.velocity = 20
             if self.rect.bottom < (SCREEN_HEIGHT - 250):
                 self.rect.y += int(self.velocity)
 
         if gameOver == False:
             if pygame.mouse.get_pressed()[0] == 1 and self.pressed == False:
                 self.pressed = True
-                self.velocity = -10
+                self.velocity = -20
             if pygame.mouse.get_pressed()[0] == 0:
                 self.pressed = False
 
@@ -86,18 +82,14 @@ class Gimble(pygame.sprite.Sprite):
                     self.index = 0
             self.image = self.sprite_frames[self.index]
 
-            self.image = pygame.transform.rotate(self.sprite_frames[self.index], self.velocity * 2)
+            self.image = pygame.transform.rotate(self.sprite_frames[self.index], self.velocity * -2)
         else:
             self.image = pygame.transform.rotate(self.sprite_frames[self.index], -90)
-
-playerGroup = pygame.sprite.Group()
-player = Gimble(int(SCREEN_WIDTH/4), int(SCREEN_HEIGHT/2))
-playerGroup.add(player)
 
 
 class Leaves(pygame.sprite.Sprite):
     def __init__(self, x_coord, y_coord, pos):
-        super().__init__()
+        pygame.sprite.Sprite.__init__(self)
 
         self.image = pygame.image.load('Assets/defaultPIPE.png')
         self.rect = self.image.get_rect()
@@ -114,8 +106,6 @@ class Leaves(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
-leafGroup = pygame.sprite.Group()
-
 
 class Button():
     def __init__(self, x_coord, y_coord, image):
@@ -127,6 +117,7 @@ class Button():
         action = False
 
         pos = pygame.mouse.get_pos()
+
         if self.rect.collidepoint(pos):
             if pygame.mouse.get_pressed()[0] == 1:
                 action = True
@@ -139,88 +130,88 @@ class Button():
 # --- Main ----------------------------------------------------------------------
 
 
-def main():
-    pygame.init()
-    pygame.display.set_caption("Flappy Gimble")
-    clock = pygame.time.Clock()
-    lastLeaf = pygame.time.get_ticks() - SPAWN_INTERVAL
-    typeface = pygame.font.SysFont("xolonium", 75)
-    button = pygame.image.load("Assets/defaultRESTART.png")
-    restartButton = Button((SCREEN_WIDTH/2) - 150, 150, button)
-    baseScroll = 0
-    score = 0
+pygame.init()
+pygame.display.set_caption("Flappy Gimble")
+clock = pygame.time.Clock()
+typeface = pygame.font.SysFont("xolonium", 75)
+bg = pygame.image.load("Assets/defaultBG.png")
+fg = pygame.image.load("Assets/defaultFG.png")
+button = pygame.image.load("Assets/defaultRESTART.png")
 
-    running = True
-    flying = False
-    gameOver = False
-    passPile = False
+baseScroll = 0
+scrollSpeed = 20
+flying = False
+gameOver = False
+lastLeaf = pygame.time.get_ticks() - SPAWN_INTERVAL
+score = 0
+passPile = False
 
-    while running:
-        clock.tick(FPS)
+playerGroup = pygame.sprite.Group()
+leafGroup = pygame.sprite.Group()
+player = Gimble(int(SCREEN_WIDTH/4), int(SCREEN_HEIGHT/2))
+playerGroup.add(player)
+restartButton = Button((SCREEN_WIDTH/2) - 150, 150, button)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.MOUSEBUTTONDOWN and flying == False and gameOver == False:
-                flying = True
+running = True
 
-        bg = pygame.image.load("Assets/defaultBG.png")
-        SCREEN.blit(bg, (0,0))
+while running:
+    clock.tick(FPS)
 
-        playerGroup.draw(SCREEN)
-        playerGroup.update()
+    SCREEN.blit(bg, (0,0))
 
-        leafGroup.draw(SCREEN)
+    playerGroup.draw(SCREEN)
+    playerGroup.update()
+
+    leafGroup.draw(SCREEN)
+
+    SCREEN.blit(fg, (baseScroll, (SCREEN_HEIGHT - 250)))
+
+    if len(leafGroup) > 0:
+        if playerGroup.sprites()[0].rect.left > leafGroup.sprites()[0].rect.left\
+            and playerGroup.sprites()[0].rect.left < leafGroup.sprites()[0].rect.right\
+                and passPile == False:
+                passPile = True
+        if passPile == True:
+            if playerGroup.sprites()[0].rect.left > leafGroup.sprites()[0].rect.right:
+                score += 1
+                passPile = False
+
+    drawText(str(score), typeface, black, int(SCREEN_WIDTH/2) - 25, 25)
+
+    if pygame.sprite.groupcollide(playerGroup, leafGroup, False, False) or player.rect.top < 0:
+        gameOver = True
+
+    if player.rect.bottom > (SCREEN_HEIGHT - 250):
+        gameOver = True
+        flying = False
+
+    if gameOver == False and flying == True:
+        timeNow = pygame.time.get_ticks()
+        if timeNow - lastLeaf > SPAWN_INTERVAL:
+            leafHeight = random.randint(-300, 100)
+            bottomLeaf = Leaves(SCREEN_WIDTH, int(SCREEN_HEIGHT/2) + leafHeight, -1)
+            topLeaf = Leaves(SCREEN_WIDTH, int(SCREEN_HEIGHT/2) + leafHeight, 1)
+            leafGroup.add(bottomLeaf)
+            leafGroup.add(topLeaf)
+            lastLeaf = timeNow
+
+        baseScroll -= scrollSpeed
+        if abs(baseScroll) > 70:
+            baseScroll = 0
+
         leafGroup.update()
 
-        fg = pygame.image.load("Assets/defaultFG.png")
-        SCREEN.blit(fg, (baseScroll, (SCREEN_HEIGHT - 250)))
+    if gameOver == True:
+        if restartButton.draw() == True:
+            gameOver = False
+            score = resetGame()
 
-        drawText(str(score), typeface, black, int(SCREEN_WIDTH/2) - 25, 25)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and flying == False and gameOver == False:
+            flying = True
 
-        if len(leafGroup) > 0:
-            if playerGroup.sprites()[0].rect.left > leafGroup.sprites()[0].rect.left\
-                and playerGroup.sprites()[0].rect.left < leafGroup.sprites()[0].rect.right\
-                    and passPile == False:
-                    passPile = True
-            if passPile == True:
-                if playerGroup.sprites()[0].rect.left > leafGroup.sprites()[0].rect.right:
-                    score += 1
-                    passPile = False
+    pygame.display.update()
 
-        if pygame.sprite.groupcollide(playerGroup, leafGroup, False, False) or player.rect.top < 0:
-            gameOver = True
-
-        if player.rect.bottom > (SCREEN_HEIGHT - 250):
-            gameOver = True
-            flying = False
-
-        if gameOver == False and flying == True:
-            timeNow = pygame.time.get_ticks()
-            if timeNow - lastLeaf > SPAWN_INTERVAL:
-                leafHeight = random.randint(-100, 100)
-                bottomLeaf = Leaves(SCREEN_WIDTH, int(SCREEN_HEIGHT/2) + leafHeight, -1)
-                topLeaf = Leaves(SCREEN_WIDTH, int(SCREEN_HEIGHT/2) + leafHeight, 1)
-                leafGroup.add(bottomLeaf)
-                leafGroup.add(topLeaf)
-                lastLeaf = timeNow
-
-            baseScroll -= scrollSpeed
-            if abs(baseScroll) > 70:
-                baseScroll = 0
-
-        if gameOver == True:
-            if restartButton.draw() == True:
-                gameOver = False
-                score = resetGame()
-
-            leafGroup.update()
-
-        pygame.display.update()
-
-        pygame.display.flip()
-
-    pygame.quit()
-
-if __name__ == "__main__":
-    main()
+pygame.quit()
